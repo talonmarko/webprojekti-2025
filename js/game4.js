1b00d1cd46f744aec959986650fb94edfa443ee1
Sari Paso <paso.sari@gmail.com>
2025-11-18 22:32:36 +0200

    Pelin toiminnallisuuden edistäminen, oikeiden ja väärien vastausten visuaalisuus

--- js/game4.js
+++ js/game4.js
@@ -8,6 +8,9 @@ const countries = document.querySelectorAll('.country')
 
 let score = 0
 
+let activeFlag = ""
+let activeCountry = ""
+
 const checkForUsername = () => {
     if (username != null) {
         welcomeMsg.textContent = `Tervetuloa pelaamaan, ${username}!`
@@ -15,48 +18,86 @@ const checkForUsername = () => {
 }
 
 const checkForMatch = (flag, country) => {
-    if (flag === country) {
-        gameScore.textContent = ++score
-        console.log('match')
-    } else {
-        console.log('no match')
+    flags.forEach(f => f.classList.remove('no-match'))
+    countries.forEach(c => c.classList.remove('no-match'))
+    
+    if (flag != "" && country != "") {
+        if (flag === country) {
+            flags.forEach(f => {
+                if (f.textContent === flag) {
+                    f.classList.add('matched')
+                    f.classList.remove('active')
+                }
+            })
+
+            countries.forEach(c => {
+                if (c.textContent === country) {
+                    c.classList.add('matched')
+                    c.classList.remove('active')
+                }
+            })
+
+            gameScore.textContent = ++score
+            activeFlag = ""
+            activeCountry = ""
+        } else if (flag != "" && country != "") {
+            flags.forEach(f => {
+                if (f.textContent === flag) {
+                    f.classList.add('no-match')
+                    f.classList.remove('active-btn')
+                }
+            })
+
+            countries.forEach(c => {
+                if (c.textContent === country) {
+                    c.classList.add('no-match')
+                    c.classList.remove('active-btn')
+                }
+            })
+
+            activeFlag = ""
+            activeCountry = ""
+        }
     }
 }
 
 const clickGameButtons = () => {
-    let activeFlag = "no active flag"
-    let activeCountry = "no active country"
-
     flags.forEach((flag, index) => flag.addEventListener('click', () => {
-        if (flag.classList.contains('active')) {
-            flag.classList.remove('active')
-        } else {
-            flag.classList.add('active')
-            activeFlag = flag.textContent
-            console.log(activeFlag)
-            for (i = 0; i < flags.length; i++) {
-                if (i != index) {
-                    flags[i].classList.remove('active')
+        if (!flag.classList.contains('matched')) {
+            if (flag.classList.contains('active-btn')) {
+                flag.classList.remove('active-btn')
+                activeFlag = ""
+            } else {
+                flag.classList.add('active-btn')
+                activeFlag = flag.textContent
+                console.log(activeFlag)
+                for (i = 0; i < flags.length; i++) {
+                    if (i != index) {
+                        flags[i].classList.remove('active-btn')
+                    }
                 }
-            }
 
-            checkForMatch(activeFlag, activeCountry)
+                checkForMatch(activeFlag, activeCountry)
+            }
         }
     }))
 
     countries.forEach((country, index) => country.addEventListener('click', () => {
-        if (country.classList.contains('active')) {
-            country.classList.remove('active')
-        } else {
-            country.classList.add('active')
-            activeCountry = country.textContent
-            for (i = 0; i < flags.length; i++) {
-                if (i != index) {
-                    countries[i].classList.remove('active')
+        if (!country.classList.contains('matched')) {
+            if (country.classList.contains('active-btn')) {
+                country.classList.remove('active-btn')
+                activeCountry = ""
+            } else {
+                country.classList.add('active-btn')
+                activeCountry = country.textContent
+                for (i = 0; i < countries.length; i++) {
+                    if (i != index) {
+                        countries[i].classList.remove('active-btn')
+                    }
                 }
-            }
 
-            checkForMatch(activeFlag, activeCountry)
+                checkForMatch(activeFlag, activeCountry)
+            }
         }
     }))
 }
