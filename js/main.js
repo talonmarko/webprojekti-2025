63715819d75e90ae71460eb0abeb33164e08449f
Jonius53 <jonius@windowslive.com>
2025-11-22 17:20:51 +0200

    Header highlightit yhteisillä sivuilla

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const userInfoContainer = document.getElementById('user-info-container')
 const username = localStorage.getItem('username')
 const welcomeMsg = document.getElementById('welcome-msg')
 
+
+
 const checkForUsername = () => {
     if (username != null) {
         welcomeMsg.textContent = `Tervetuloa pelaamaan, ${username}!`
