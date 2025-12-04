690a816dd1a107bf01f315a6576f604c2be650d1
tuippa <tuire.huutonen@hotmail.com>
2025-12-04 11:17:08 +0200

    Hamburger menu on navbar and function to go with it

--- js/main.js
+++ js/main.js
@@ -68,8 +68,23 @@ const activePage = () => {
     
 }
 
-activePage()
+//Hampurilaisvalikon avaaminen ja sulkeminen
 
+const hamburgerMenu = () => {
+    const hamButton = document.getElementById("hamburger-button")
+    const hamMenu = document.getElementById("hamburger-menu")
+
+    hamButton.addEventListener("click", () => {
+        if (hamMenu.classList.contains("hidden")) {
+            hamMenu.classList.remove("hidden")
+        } else {
+            hamMenu.classList.add("hidden")
+        }
+        
+    })
+}
+
+hamburgerMenu()
 
 activePage()
 
